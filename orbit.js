fb14f522514ff45c1f8c814ccfc63e89550ed4c1
rylanp <91034086+rylanp@users.noreply.github.com>
2021-09-22 18:58:44 -0500

    g

--- orbit.js
+++ orbit.js
@@ -1,10 +1,15 @@
 //#region F = 9.8 * (mass1 * mass2) / (sqrdistance)
 let mouseX = 0;
 let mouseY = 0;
+
+let ball = document.getElementById('ball');
 let circle = document.getElementById('cursor');
+
 const onMouseMove = (e) =>{
   circle.style.left = e.pageX + 'px';
   circle.style.top = e.pageY + 'px';
+  mouseX = e.pageX;
+  mouseY = e.pageY;
   console.log("X: " + e.pageX);
   console.log("Y: " + e.pageY);
 }
