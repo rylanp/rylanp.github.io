98fa5400592a87e51826b586fb383ddf283d8f7d
rylanp <91034086+rylanp@users.noreply.github.com>
2021-09-22 18:56:10 -0500

    Update orbit.js

--- orbit.js
+++ orbit.js
@@ -1,7 +1,7 @@
 //#region F = 9.8 * (mass1 * mass2) / (sqrdistance)
 let mouseX = 0;
 let mouseY = 0;
-let circle = document.getElementById('circle');
+let circle = document.getElementById('cursor');
 const onMouseMove = (e) =>{
   circle.style.left = e.pageX + 'px';
   circle.style.top = e.pageY + 'px';
