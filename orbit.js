66f027a7fb7b045dae081e8bfc0bf7280af32351
rylanp <91034086+rylanp@users.noreply.github.com>
2021-09-22 18:54:21 -0500

    stuff

---
+++ orbit.js
@@ -0,0 +1,11 @@
+//#region F = 9.8 * (mass1 * mass2) / (sqrdistance)
+let mouseX = 0;
+let mouseY = 0;
+let circle = document.getElementById('circle');
+const onMouseMove = (e) =>{
+  circle.style.left = e.pageX + 'px';
+  circle.style.top = e.pageY + 'px';
+  console.log("X: " + e.pageX);
+  console.log("Y: " + e.pageY);
+}
+document.addEventListener('mousemove', onMouseMove);
